82920d9e2e47ed45bbe0cde767ccad2f25e2275b
Jon Leete <jleete97@gmail.com>
2016-03-13 18:54:33 -0400

    get weather working

--- WebContent/html/js/weather.js
+++ WebContent/html/js/weather.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	
 	$("#refreshWeather").click(function() {
+		return;
 		alert("Clicked!");
 		var zip = "22101";
 		var start = "2016-01-10T00:00:00";
@@ -38,4 +39,40 @@ $(document).ready(function() {
 					});
 		});
 	});
+	
+	var updateConditions = function(data) {
+		var obs = data.current_observation;
+		
+		$("#weatherWhere").text(obs.full);
+		$("#currentConditionsIcon").attr("src", obs.icon_url);
+		$("#currentConditions").text(obs.weather);
+		$("#currentTemp").text(obs.temp_f + " F");
+	};
+	
+	var updateForecast = function(data) {
+		var day = data.forecast.simpleforecast.forecastday[0];
+		
+		$("#forecastHigh").text("" + day.high.fahrenheit + " F");
+		$("#forecastLow").text("" + day.low.fahrenheit + " F");
+		$("#forecastIcon").attr("src", day.icon_url);
+		$("#forecastConditions").text(day.conditions);
+	}
+	
+	var getWundergroundWeather = function() {
+		var zip = "22101";
+		var key = "110eef78ce81b428";
+		var urlBase = "http://api.wunderground.com/api";
+		
+		var currentUrl = buildUrl(urlBase, key, zip, "conditions");
+		$.getJSON(currentUrl, updateConditions);
+		
+		var forecastUrl = buildUrl(urlBase, key, zip, "forecast");
+		$.getJSON(forecastUrl, updateForecast);
+	};
+	
+	var buildUrl = function(base, key, zip, type) {
+		return base + "/" + key + "/" + type + "/q/" + zip + ".json";
+	}
+	
+	$("#refreshWeather").click(getWundergroundWeather);
 });
